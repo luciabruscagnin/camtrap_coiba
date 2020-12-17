daec096edbb3db1c5b48e745b46a60bcfc6baf06
Brendan Barrett <bbarrett@ab.mpg.de>
2020-12-17 15:35:21 +0100

    exiftool scripts, agouti sequence storage scripts

---
+++ agouti_sequence_collection.R
@@ -0,0 +1,190 @@
+# d <- data.frame(matrix(ncol = 4, nrow = 0))
+# x <- c("agouti_sequence", "image_id", "individual" , "notes")
+# colnames(d) <- x
+# d
+# d[norw(d+1),] <- c(https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/1479a34c-f9d5-4697-b26d-58b9372c7510,
+#                    ,
+#                    ,
+#                    )
+# agouti <- function(sequence=0 , image_id=0 , individual=0 , notes=0){
+#      
+# }
+# 
+# d <- data.frame(matrix(ncol = 2, nrow = 0))
+# x <- c("agouti_sequence", "image_id", "individual" , "notes")
+# colnames(d) <- x
+# 
+# agouti <-
+#      function(...) {
+#           
+#           url <- as.character(readline(prompt="Paste the Agouti Url Here: "))
+#           seq <- as.numeric(readline(prompt="What is the sequence number?: "))
+#           #id <- as.character(readline(prompt="Who is the individual?"))
+#           #com <- as.character(readline(prompt="Additional comments"))
+#           print( cbind(url,seq) )
+#      }
+#           
+#    
+# agouti()       
+
+d <- data.frame(matrix(ncol = 4, nrow = 0))
+x <- c("agouti_sequence", "image_id", "individual" , "notes")
+colnames(d) <- x
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                      , 195 ,
+                      "ABE" , 
+                      NA
+                      )
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/1479a34c-f9d5-4697-b26d-58b9372c7510"
+                       , 124 ,
+                       "ABE" , 
+                       NA
+)
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/496dd7b9-9121-4c29-b3af-c79f4d1b2668"
+                       , 8 ,
+                       "ABE" , 
+                       NA
+)
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                       , 369 ,
+                       "SMG" , 
+                       NA
+)
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                       , 369 ,
+                       "SMG" , 
+                       NA
+)
+
+     
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/1479a34c-f9d5-4697-b26d-58b9372c7510"
+                       , 259 ,
+                       "SMG" , 
+                       NA
+)     
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                       , 190 ,
+                       "TOM" , 
+                       "standing behind juvenile"
+) 
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/1479a34c-f9d5-4697-b26d-58b9372c7510"
+                       , 124 ,
+                       "TOM" , 
+                       NA
+) 
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/40043c07-31d3-4db9-9941-039839a8a1b0"
+                       , 22 ,
+                       "TOM" , 
+                       NA
+) 
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                       , 558 ,
+                       "CYS" , 
+                       NA
+)    
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/e2b46e04-5933-44a8-907e-e914a05655c9"
+                       , 558 ,
+                       "CYS" , 
+                       NA
+)    
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/a3918cc9-2c3c-4302-b52b-20206950ee02"
+                       , 1 ,
+                       "CYS" , 
+                       NA
+)
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/e657fcd0-9141-40cf-9270-7c4e73fc20ec"
+                       , 1 ,
+                       "MRE" , 
+                       NA
+)   
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                       , 193 ,
+                       "BEA" , 
+                       NA
+)   
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/1479a34c-f9d5-4697-b26d-58b9372c7510"
+                       , 124 ,
+                       "BEA" , 
+                       NA
+)   
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/f256a3fa-7f73-455a-b2ee-3a288a4e8ee4"
+                       , 1 ,
+                       "BEA" , 
+                       NA
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/f256a3fa-7f73-455a-b2ee-3a288a4e8ee4"
+                       , 1 ,
+                       "BEA" , 
+                       NA
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/91502947-533d-4347-a05f-74bff21b5670"
+                       , 140 ,
+                       "LEO" , 
+                       "with infant and juvenile"
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/982c3760-80b9-42a5-a228-b90ddf6a8ea5"
+                       , 17 ,
+                       "LEO" , 
+                        NA                       
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                       , 52 ,
+                       "LEO" , 
+                       NA                       
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/5583d3eb-49c1-467d-b738-cabb60064fea"
+                       , 52 ,
+                       "LEO" , 
+                       NA                       
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/9a05da25-8886-4124-bfe3-f076c8727f23"
+                       , 161 ,
+                       "DOT" , 
+                       "snoutier than LEO"                       
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/1479a34c-f9d5-4697-b26d-58b9372c7510"
+                       , 80 ,
+                       "DOT" , 
+                       "snoutier than LEO"                       
+)  
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/1479a34c-f9d5-4697-b26d-58b9372c7510"
+                       , 178 ,
+                       "SAD" , 
+                       NA                       
+) 
+
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/a3e11e2f-b2a6-425b-9b54-3c38fab8d930"
+                       , 1 ,
+                       "SAD" , 
+                       NA                       
+) 
+
+###########NEED INSPECTION
+d[nrow(d) + 1 , ] <- c("https://www.agouti.eu/#/project/0e0508af-d3dc-4be6-9806-dbdba5ec3368/annotate/sequence/4c037780-57e1-41cf-b82e-aa866ad67eb8"
+                       , 33 ,
+                       "LEO" , 
+                       NA                       
+)  
---
+++ exiftool_lessons.sh
@@ -0,0 +1,62 @@
+#accident 279.94 per annum
+173.16 
+
+303 570 0370
+
+1) Set system of equations and convert to frequencies.
+2) Compute selection gradients of each strategy with reference to l and L
+	b) solve for q hat at steady state
+	a) solve stationary demography, which is stable age distribution of the frequency of s,h,N,
+	A. This is the dominant eigenvector, if using a matric aka Caswel approach.
+    c) calculate lambda, the growth rate of each strategy take partial derivatives with respect to L and l
+3) Plot evolutionary dynamics of s and h over time
+
+
+#shell scrpt from brett to rename files by metadata
+
+#in the terminal set the working directory to the location of the JPG and run
+
+
+cd "/Users/BJB/Desktop/exiftool_test/"
+#below is brett code for images
+exiftool -r '-filename<${datetimeoriginal}_${model;}%-c.%e' -d "%Y%m%d_%H%M%S" .
+
+#my attempt
+exiftool -r '-filename<${CreateDate}_${UserLabel;}%-c.%e' -d "%Y%m%d_%H%M%S" .
+#read 
+exiftool RCNX0012.MP4
+exiftool -s -G RCNX0012.MP4
+
+
+exiftool -g1 -a -s -CreateDate -TrackCreateDate RCNX0012.MP4
+exiftool -g1 -a -s -CreateDate RCNX0012.MP4
+exiftool -g1 -a -s -UserLabel RCNX0012.MP4
+
+##makes file  YYYYMMDD_HHMMSS_FILENAME_ #double extensions though
+exiftool -r '-filename<${CreateDate}_${FileName;}%-c.%e' -d "%Y%m%d_%H%M%S" .
+exiftool -r '-filename<${CreateDate}_${FileName;}%-c.' -d "%Y%m%d_%H%M%S" .
+###for videos
+# rename file with created date, name, and YYMMDDHHMMSS
+exiftool -r '-filename<${CreateDate}_${FileName;}%-c.' -d "%Y%m%d_%H%M%S" .
+####adds copyright and artist to all files in wd
+
+exiftool -r -overwrite_original -artist="Brendan J Barrett" -copyright="Max Planck Institue of Animal Behavior" -comment="CEBUS-01-R1" .
+#below is very close, we need to put dashes in between files names,, or not
+exiftool -r '-filename<${Comment}_${CreateDate}%-c.%e' -d "%Y%m%d_%H%M%S" .
+
+
+exiftool -r '-filename<${CreateDate}_${FileName;}%-c.%e.' -d "%Y%m%d_%H%M%S" .
+
+
+exiftool -r '-filename<${CreateDate}_${FileName;}%-c.%e' -d "%Y%m%d_%H%M%S" .
+
+
+exiftool -r '-filename<${CreateDate}_${FileName;}%-c.%e' -d "%Y%m%d_%H%M%S" .
+
+exiftool -r "-filename<CreateDate" -d "%Y-%m-%d_%H-%M-%S - %%f.%%e" RCNX0012.MP4
+
+exiftool -r '-filename<${CreateDate}_${%dogs%}%-c.%e' -d "%Y%m%d_%H%M%S" .
+
+
+###email w/ info, prev convos, hints, scopes, time commitment
+
