fd7cdc70f8b6a19aacb8743fa191663541e694de
Brendan Barrett <bbarrett@ab.mpg.de>
2020-12-30 10:41:14 +0100

    added gps viz, need to deal with gpx files

---
+++ coiba_camtrap_gps_visualization.R
@@ -0,0 +1,148 @@
+library(mapview)
+library(sf) 
+library(rgdal)
+library(lubridate)
+
+#setwd("/Users/BJB/Dropbox/Coiba Tool Images/GPX/")
+
+
+cammar2017 <- readOGR(dsn = "~/Dropbox/Capuchin Monkeys, Coiba National Park_July2018/GPS Points/Camera Deployments Mar 2017.GPX", layer="waypoints")
+camjul2017 <- readOGR(dsn = "~/Dropbox/Capuchin Monkeys, Coiba National Park_July2018/GPS Points/Camera Deployments July 2017.GPX", layer="waypoints")
+camjan2018 <- readOGR(dsn = "~/Dropbox/Capuchin Monkeys, Coiba National Park_July2018/GPS Points/Camera Deployments Jan 2018.GPX", layer="waypoints")
+
+mccir <- readOGR(dsn ="~/Dropbox/Capuchin Monkeys, Coiba National Park_July2018/GPS Points/MCCinreachJan2020.GPX" , layer="waypoints")##inreaches need a space on xml code in first line removed to code
+m <- mccir[1:756,]
+m2019 <- m[year(m$time)>2018,]
+str(m2019)      
+mapview(m2019[19,])
+sort(m$name)
+mapview(cammar2017 , col.region="red") + mapview(camjul2017 , col.region="orange") + mapview(camjan2018 , col.region="green") + mapview(m2019 , col.region="blue")
+mapview(m2019 , col.region="blue")
+#######interactive maps#######
+
+##edit mar 2017 deployment
+cammar2017$old_name <- cammar2017$name
+cammar2017$name <- as.character(cammar2017$name)
+cammar2017$name[1] <-  "SURVEY-CEBUS-01-01-R1"
+cammar2017$name[2] <-  "SURVEY-CEBUS-02-01-R1"
+cammar2017$name[3]  <- "SURVEY-CEBUS-02-02-R1"
+cammar2017$name[4] <- "SURVEY-CEBUS-03-01-R1"
+cammar2017$name[5] <- "SURVEY-CEBUS-03-02-R1"
+cammar2017$name[6]  <- "SURVEY-CEBUS-04-01-R1"
+cammar2017$name[7] <- "SURVEY-CEBUS-05-01-R1"
+cammar2017$name[8] <- "SURVEY-CEBUS-06-01-R1"
+cammar2017$name[9] <- "CEBUS-01-R1"
+cammar2017$name[10] <- "CEBUS-02-R1"
+cammar2017$name[11] <- "SURVEY-CEBUS-08-01-R1"
+cammar2017$name[12] <- "SURVEY-CEBUS-09-01-R1"
+cammar2017$name[13] <- "SURVEY-CEBUS-09-02-R1"
+cammar2017$name[14] <- "SURVEY-CEBUS-10-01-R1"
+cammar2017$name[15] <- "SURVEY-CEBUS-11-01-R1"
+cammar2017$name[16] <- "SURVEY-CEBUS-12-01-R1"
+
+mapview(cammar2017 , zcol='name')
+
+##edit july 2017 deployment
+camjul2017$old_name <- camjul2017$name
+camjul2017$name <- as.character(camjul2017$name)
+mapview(camjul2017 , zcol='name')
+camjul2017$name
+camjul2017$name[1] <- "SURVEY-CEBUS-03-03-R2"
+camjul2017$name[2] <- "SURVEY-CEBUS-06-01-R2"
+camjul2017$name[3] <- "CEBUS-01-R2"
+camjul2017$name[4] <- "CEBUS-02-R2"
+camjul2017$name[5] <- "SURVEY-CEBUS-13-01-R2"
+camjul2017$name[6] <- "SURVEY-CEBUS-14-01-R2"
+camjul2017$name[7] <- "SURVEY-CEBUS-15-01-R2"
+camjul2017$name[8] <- "SURVEY-CEBUS-16-01-R2"
+camjul2017$name[9] <- "CEBUS-08-R2"
+camjul2017$name[10] <- "CEBUS-09-R2"
+camjul2017$name[11] <- "SURVEY-CEBUS-18-01-R2"
+camjul2017$name[12] <-  "SURVEY-CEBUS-19-01-R2"
+camjul2017$name[13] <- "SURVEY-CEBUS-20-01-R2"
+camjul2017$name[14] <- "SURVEY-CEBUS-21-01-R2"
+camjul2017$name[15] <- "SURVEY-CEBUS-06-02-R2"
+
+
+##ADD MISSING CAMERAS
+
+camjul2017 <- rbind(camjul2017,cammar2017[16,])
+camjul2017$name[16] <-"SURVEY-CEBUS-12-01-R2"
+
+####add below still
+#CEBUS-05-R2
+#SURVEY-CEBUS-16-02-R2
+
+
+
+
+
+
+str(camjul2017)
+mapview(camjul2017 , zcol='name')
+
+
+
+#########jan 2018
+
+
+camjan2018$old_name <- camjan2018$name
+camjan2018$name <- as.character(camjan2018$name)
+mapview(camjan2018 , zcol='name')
+camjan2018$name
+camjan2018$name[1] <- "SURVEY-CEBUS-03-04-R3"
+camjan2018$name[3] <- "CEBUS-01-R3"
+camjan2018$name[4] <- "CEBUS-02-R3"
+camjan2018$name[5] ####whats up with this
+camjan2018$name[6] <- "SURVEY-CEBUS-15-02-R3"
+camjan2018$name[7] <- "SURVEY-CEBUS-15-03-R3"
+camjan2018$name[8] <- "CEBUS-05-R3"
+camjan2018$name[9] <- "CEBUS-08-R3"
+camjan2018$name[10] <- "SURVEY-CEBUS-22-01-R3"
+camjan2018$name[11] <- "SURVEY-CEBUS-23-01-R3"
+
+camjan2018 <- camjan2018[-c(2,5,12),]
+
+camjan2018  <- rbind(camjan2018,camjan2018[2,])
+camjan2018$name
+camjan2018$name[10] <- "SURVEY-CEBUS-07-03-R3"
+
+camjan2018  <- rbind(camjan2018,cammar2017[1,])
+
+camjan2018$name[11] <- "SURVEY-CEBUS-01-02-R4"
+
+mapview(camjan2018 , zcol='name')
+
+#need to add 
+# SURVEY-CEBUS-17-03-R3
+camjan2018$name
+str(camjan2018)
+camjan2018$name[1]
+camjan2018@coords[1,]
+camjan2018$name[2]
+camjan2018@coords[2,]
+camjan2018$name[3]
+camjan2018@coords[3,]
+camjan2018$name[4]
+camjan2018@coords[4,]
+camjan2018$name[5]
+camjan2018@coords[5,]
+camjan2018$name[6]
+camjan2018@coords[6,]
+camjan2018$name[7]
+camjan2018@coords[7,]
+camjan2018$name[8]
+camjan2018@coords[8,]
+camjan2018$name[9]
+camjan2018@coords[9,]
+camjan2018$name[10]
+camjan2018@coords[10,]
+camjan2018$name[11]
+camjan2018@coords[11,]
+camjul2017$name[10]
+camjul2017@coords[10,]
+
+camjan2018$name[8]
+camjan2018@coords[8,]
+
+cbind(cammar2017$name,cammar2017@coords)
