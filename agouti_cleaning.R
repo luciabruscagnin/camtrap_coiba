5b97c04d6904708fb70d92c3307e5f264375d6d8
Zoë <zgoldsborough@outlook.com>
2021-05-26 11:48:13 +0200

    working on cleaning agouti data

--- agouti_cleaning.R
+++ agouti_cleaning.R
@@ -1,6 +1,7 @@
 # CLEANING AGOUTI OUTPUT
 require(dplyr)
 require(lubridate)
+require(stringr)
 
 # open Agouti output file (observations)
 agoutigross <- read.csv("agouti_output/coiba-national-park-tool-use-20210518123935/observations.csv", header = TRUE)
@@ -44,10 +45,49 @@ length(unique(agoutisequence$sequence_id))
 agoutisequence$time <- str_replace(agoutisequence$timestamp, "T", " ")
 agoutisequence$time <- as.POSIXct(agoutisequence$time, tz = "America/Panama", format = "%Y-%m-%d %H:%M:%S")
 
-agoutisequence$tidedif <- NA
-which(is.na(agoutisequence$time))
+# identify wrong timestamps
+# multimedia excel with correct timestamps
+multimedia <- read.csv("agouti_output/coiba-national-park-tool-use-20210518123935/multimedia.csv", header = TRUE)
+
+# first those that were just entry mistakes (e.g. 1970)
+# create dataset with these sequence ids
+wrongseq <- agoutisequence$sequence_id[agoutisequence$timestamp < 2017-01-01 | agoutisequence$timestamp > Sys.Date()]
+
+# extract rows of multimedia excel with the same sequence ids
+multimedia2 <- subset(multimedia, multimedia$sequence_id %in% wrongseq)
+
+# use stringr to get the correct timestamp
+# overwrite those in the old one?
+multimedia2$date <- sapply(str_split(multimedia2$file_name, "__"), '[', 2) 
+multimedia2$time <- sapply(str_split(multimedia2$file_name, "__"), '[', 3) 
+multimedia2$time <- substr(multimedia2$time, 1, nchar(multimedia2$time)-4)
+multimedia2$timestamp_correct <- as.POSIXct(paste(multimedia2$date, multimedia2$time), tz = "America/Panama", format = "%Y-%m-%d %H-%M-%S")
+
+#make new time column
+agoutisequence$time[which(agoutisequence$sequence_id %in% multimedia2$sequence_id)] <- multimedia2$timestamp_correct
+which(agoutisequence$sequence_id %in% multimedia2$sequence_id)
+agoutisequence[which(agoutisequence$sequence_id %in% multimedia2$sequence_id),]
+for (i in 1:nrow(multimedia2)) {
+  if (agoutisequence$sequence_id == multimedia2$sequence_id[i]) {
+    agoutisequence$time <- multimedia2$timestamp_correct
+  }
+}
+# then look for one's that are around midnight (00:00:00) to see if there's a mistake there
+str(agoutisequence$time)
+# need to fix deployment start variable too (at 1970 now) and date end.... 
+agoutisequence$timeonly <- format(agoutisequence$time, format = "%H:%M:%S")
+wrongseq2 <- agoutisequence$sequence_id[agoutisequence$timeonly == "00:00:00"]
+multimedia3 <- subset(multimedia, multimedia$sequence_id %in% wrongseq2)
+
+
+agoutisequence[,agoutisequence$timeonly == 00:00:00]
+agoutisequence
+## TIDAL
+
 
 # for each sequence get time to nearest low tide (need to match day and get low tide times then)
+agoutisequence$tidedif <- NA
+which(is.na(agoutisequence$time))
 
 for (i in 1:nrow(agoutisequence)) {
   agoutisequence$tidedif[i] <- min(abs(difftime(agoutisequence$time[i], TidesLow$TIDE_TIME, units = "hours")))
@@ -64,3 +104,9 @@ hist(h.lub)
 
 # for each camera trap add get distance from coast
 str(agoutisequence2)
+
+
+
+### exploring
+
+# loop over camera id make density plot for each camera (potentially only with capuchin present)
