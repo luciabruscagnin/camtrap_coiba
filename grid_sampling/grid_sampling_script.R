1fd0283bce803a8faaf1aa132f9c35f9fe284603
Zoë <zgoldsborough@outlook.com>
2021-07-02 15:33:16 +0200

    start of grid sampling script

---
+++ grid_sampling/grid_sampling_script.R
@@ -0,0 +1,35 @@
+#### Grid for field trip 02.07.2021 ####
+## Grid in Jicaron tool use site, 20-25 cameras
+# ~ 250 m spacing
+
+# first run coiba_map_gps_viz.R to create all the layers for the maps
+library(mapview)
+library(rgdal)
+library(sf)
+library(raster)
+
+
+
+
+
+
+
+
+#maybe use gridshapes
+
+grid_shapes <- rbind(tools_w201707201901,tools_w201803,tools_w201807) # define grid area using a subset of tools w/ same number columns
+str(grid_shapes)
+e <- as(raster::extent(min(grid_shapes@coords[,1]), max(grid_shapes@coords[,1]), min(grid_shapes@coords[,2]), max(grid_shapes@coords[,2])), "SpatialPolygons")
+proj4string(e) <- crs(grid_shapes)
+e3 <- spTransform(e, CRS("+init=EPSG:32616"))
+ebuf <- buffer(e3, width = 1500) #add 500 m buffer
+e2 <- st_as_sf(e)
+e2b <- st_as_sf(ebuf)
+grid_1000m <- st_make_grid(e2b, square = T, cellsize = c(1000, 1000) ) %>% 
+  st_sf() #100m grid
+grid_500m <- st_make_grid(e2b, square = T, cellsize = c(500, 500) ) %>% 
+  st_sf() #100m grid
+grid_250m <- st_make_grid(e2b, square = T, cellsize = c(250, 250)) %>% # the grid, covering bounding box
+  st_sf() # not really required, but makes the grid nicer to work with later
+
+all_tools_map + all_streams_map +  most_almendras_map + all_cams_map + all_tracks + mapview(grid_250m, col.regions = "white")
