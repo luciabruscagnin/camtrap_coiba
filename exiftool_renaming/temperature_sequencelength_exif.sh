2d29eb23e03fc7693e66e388f6507ac51e8b7af5
Brendan Barrett <bbarrett@ab.mpg.de>
2021-11-09 13:29:07 +0100

    r4 and r5 temp exif

--- exiftool_renaming/temperature_sequencelength_exif.sh
+++ exiftool_renaming/temperature_sequencelength_exif.sh
@@ -3,3 +3,9 @@
 exiftool  -r -csv -FileName -CreateDate -MediaDuration -AmbientTemperature -d %d/%m/%Y_%H:%M:%S /Volumes/Coiba\ Image\ Data/renamed_data/R1    > /Users/sifaka/Dropbox/camtrap_coiba/exiftool_metadata/temperature_seqlength_exif/r1_temperature_sequencelength.csv 
 #r2
 exiftool  -r -csv -FileName -CreateDate -MediaDuration -AmbientTemperature -d %d/%m/%Y_%H:%M:%S /Volumes/Coiba\ Image\ Data/renamed_data/R2    > /Users/sifaka/Dropbox/camtrap_coiba/exiftool_metadata/temperature_seqlength_exif/r2_temperature_sequencelength.csv 
+#r3
+exiftool  -r -csv -FileName -CreateDate -MediaDuration -AmbientTemperature -d %d/%m/%Y_%H:%M:%S /Volumes/Coiba\ Image\ Data/renamed_data/R3    > /Users/sifaka/Dropbox/camtrap_coiba/exiftool_metadata/temperature_seqlength_exif/r3_temperature_sequencelength.csv 
+#r4
+exiftool  -r -csv -FileName -CreateDate -MediaDuration -AmbientTemperature -d %d/%m/%Y_%H:%M:%S /Volumes/Coiba\ Image\ Data/renamed_data/R4    > /Users/sifaka/Dropbox/camtrap_coiba/exiftool_metadata/temperature_seqlength_exif/r4_temperature_sequencelength.csv 
+#r5
+exiftool  -r -csv -FileName -CreateDate -MediaDuration -AmbientTemperature -d %d/%m/%Y_%H:%M:%S /Volumes/Coiba\ Image\ Data/renamed_data/R5    > /Users/sifaka/Dropbox/camtrap_coiba/exiftool_metadata/temperature_seqlength_exif/r5_temperature_sequencelength.csv 
