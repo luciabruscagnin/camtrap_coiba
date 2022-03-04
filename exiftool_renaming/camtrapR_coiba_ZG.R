af71f048ee7d1a6ac0511e9bbeb6fbddb88325ea
Zoë <zgoldsborough@outlook.com>
2022-03-04 17:29:55 +0100

    how to do exiftool and camptrap coiba on windows (Zoë)

---
+++ exiftool_renaming/camtrapR_coiba_ZG.R
@@ -0,0 +1,26 @@
+library(camtrapR)
+
+### Set working directory to external harddrive that contains the raw files
+## all jpgs have to be in one folder, no subfolders (so manually copy the files from the 100RECNX folders into the main folder)
+setwd("D:/")
+
+# need directory structure DeploymentNumber/Locations. E.g. R10 folder containing CEBUS-02,CEBUS-04 etc
+# IMPORTANT: Make sure all the folders are already fully labeled with their final name, so CEBUS-02-R10. 
+
+# say which directory is the 'station' directory 
+wd_createStationDir <- file.path("/Practice", "R10")
+# define where the raw data is
+wd_images_raw<- file.path("/Practice/R10")
+# define where the renamed data has to go
+wd_images_raw_renamed <- file.path("/Practice/Renamed/R10")
+
+# rename the entire folder of R10
+# this will rename all the JPGs, not the MP4s!!
+renaming.table <- imageRename(inDir               = wd_images_raw, #source path
+                              outDir              = wd_images_raw_renamed, #sink path
+                              hasCameraFolders    = FALSE, #if old files have camera subfolder, need to be set to false
+                              copyImages          = TRUE, #if copying will actually happen, set to false to simulate renaminf
+                              keepCameraSubfolders = FALSE,
+                              writecsv = TRUE)
+
+# after this then run the exiftool shellscript also in this folder manually per video location to rename the MP4s
