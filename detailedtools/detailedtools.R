2dc6bd6d65f18daef9617937d2e466d8503a96c5
Zoë Goldsborough <zgoldsborough@outlook.com>
2023-01-19 16:56:19 +0100

    doing some more initial detailed tools analyses for meeting with Meg

--- detailedtools/detailedtools.R
+++ detailedtools/detailedtools.R
@@ -9,6 +9,11 @@ library(dplyr)
 library(tidyr)
 library(brms)
 library(ggplot2)
+require(tidybayes)
+library(fitdistrplus)
+
+
+## NOTE: add code about catching when anvil type switches (e.g. if it's wooden anvil at comment seq_start, or whe nthey switch back to stone/wood)
 
 # load tsv file with aggregated BORIS output
 dettools <- read.csv("detailedtools/ZGdetailedtoolscoding.tsv", sep = "\t")
@@ -114,8 +119,6 @@ dettools_r2$mistaketype <- ifelse(dettools_r2$behavior == "misstrike", dettools_
 # specify repositioning type
 dettools_r2$repostype <- ifelse(dettools_r2$behavior == "reposit", dettools_r2$modifiers, NA)
 
-
-
 # will have to do when there's a hammerswitch that hammerID changes until seq_end (use for loop for it like above)
 currenthammerID <- dettools_r2$hammerID[1]
 
@@ -134,6 +137,31 @@ for (i in 1:nrow(dettools_r2)) {
 
 # extract location of hammerstone they switched to 
 dettools_r2$h_switchloc <- ifelse(dettools_r2$behavior == "hammerswitch", sapply(str_split(dettools_r2$modifiers, "[|]"), '[', 1), NA)
+
+## incorporate what type of anvil it is
+# default is stone, if it was wood we made a comment at the seqstart
+seqdat$anviltype <- ifelse(seqdat$sequenceID %in% dettools_r2$sequenceID[which(dettools_r2$behavior == "seqstart" & str_detect(dettools_r2$comment, "wood") == TRUE)], "wood", "stone")
+# attach this to main dataframe
+dettools_r2 <- left_join(dettools_r2, seqdat[,c("sequenceID", "anviltype")], "sequenceID")
+
+# will have to do when there's an anvilswitch that anviltype changes until seq_end (use for loop for it like above)
+currentanviltype <- dettools_r2$anviltype[1]
+
+for (i in 1:nrow(dettools_r2)) {
+  dettools_r2$anviltype2[i] <- currentanviltype
+  if(dettools_r2$behavior[i] == "anvilswitch") {
+    currentanviltype <- dettools_r2$modifiers[i]
+    dettools_r2$anviltype2[i] <- currentanviltype
+  }
+  
+  if(dettools_r2$behavior[i] == "seqstart") {
+    currentanviltype <- dettools_r2$anviltype[i]
+    dettools_r2$anviltype2[i] <- currentanviltype
+  }
+}
+
+## so anviltype2 and hammerID2 are the correct variables
+
 
 # calculate duration 
 # need to extract the start and end time of the videos
@@ -206,12 +234,24 @@ dettools_r2$Sex[which(is.na(dettools_r2$Sex) == TRUE)] <- ifelse(str_detect(dett
                                                                  ifelse(str_detect(dettools_r2$subjectID[which(is.na(dettools_r2$Sex) == TRUE)], "female") == TRUE, "Female", "Unknown")) 
 # have some extra  spaces that snuck in
 dettools_r2$Age <- str_trim(dettools_r2$Age)
+dettools_r2$hammerID2 <- str_trim(dettools_r2$hammerID2)
+
 
 # sequence level dataframe (only for analyzing things like nr of pounds/duration. things that are fixed per sequence)
 detseq <- dettools_r2[!duplicated(dettools_r2$sequenceID),]
+detseq$split <- seqdat$split[which(detseq$sequenceID == seqdat$sequenceID)]
+
+#saveRDS(detseq, "detailedtools/RDS/detseq.rds")
 
 ####### INITIAL ANALYSES/MESSING AROUND ########
 ## move this to a different script later
+
+### IF SEQUENCE IS SPLIT then we can "reliably" still look at the duration of the sequence in seconds (tho likely overestimation?)
+# but to be safe probably shouldnt include these when looking at number of pounds as we def miss at least 1 maybe more pounds in between
+# maybe exclude them in general if we have enough data, think about it
+# definitely include whether it was split yes/no as a random effect/fixed effect 
+
+## add itemtype as well 
 
 # Age differences in efficiency (duration of sequence)
 # filter to only opened sequences
@@ -225,39 +265,207 @@ detseq_o$age_of <- factor(detseq_o$Age, ordered = TRUE, levels = c("Juvenile", "
 ggplot(detseq_o, aes(x=age_of, y=seqduration)) + 
   geom_violin()
 
-m_e1 <- brm(seqduration ~ age_of, data = detseq_o)
+descdist(detseq_o$seqduration)
+
+testdist1.1 <- fitdist(detseq_o$seqduration, "norm")
+plot(testdist1.1)
+
+testdist1.2 <- fitdist(detseq_o$seqduration, "gamma")
+plot(testdist1.2)
+
+
+m_e1 <- brm(seqduration ~ Age + item + split, data = detseq_o, iter = 1000, chain = 2, core = 2, backend = "cmdstanr", family = "gamma")
+#saveRDS(m_e1, "detailedtools/RDS/m_e1.rds")
 summary(m_e1)
 mcmc_plot(m_e1)
+pp_check(m_e1)
 plot(conditional_effects(m_e1))
 
+# make violin plot
+m_type_pred <- m_e1 %>% 
+  epred_draws(newdata = tibble(item = detseq_o$item,
+                               Age = detseq_o$Age,
+                               split = detseq_o$split))
+
+# age difference in duration to open item
+ggplot(data = m_type_pred, aes(x = Age, y = .epred)) + geom_violin(aes(color = Age, fill = Age), alpha = 0.4) + ylim(0,50) +
+  stat_summary(detseq_o, inherit.aes = FALSE, mapping=aes(x = Age, y = seqduration, color = Age), geom = "point", fun = "mean",
+               size = 4) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  guides(color = "none", fill = "none") +
+  labs(x = "Age", y = "Seconds required to open item") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
+# item difference in duration to open item
+ggplot(data = m_type_pred, aes(x = item, y = .epred)) + geom_violin(aes(color = item, fill = item), alpha = 0.4) + ylim(0,100) +
+  stat_summary(detseq_o, inherit.aes = FALSE, mapping=aes(x = item, y = seqduration, color = item), geom = "point", fun = "mean",
+               size = 4) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  guides(color = "none", fill = "none") +
+  labs(x = "Item type", y = "Seconds required to open item") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
+## including interaction of age and item (once we have more reliable data for each item type)
+#m_e1b <- brm(seqduration ~ age_of + item + age_of*item + split, data = detseq_o, iter = 1000, chain = 2, core = 2, backend = "cmdstanr", family = "gamma")
+plot(conditional_effects(m_e1b))
+
+
 ## Age differences in efficiency (number of pounds)
-boxplot(detseq_o$n_pounds ~ detseq_o$age_of)
-ggplot(detseq_o, aes(x=age_of, y=n_pounds)) + 
+boxplot(detseq_o$n_pounds ~ detseq_o$Age)
+ggplot(detseq_o, aes(x=Age, y=n_pounds)) + 
   geom_violin()
 
-m_e2 <- brm(n_pounds ~ age_of, data = detseq_o)
+descdist(detseq_o$n_pounds)
+
+testdist2.1 <- fitdist(detseq_o$n_pounds, "pois")
+plot(testdist2.1)
+
+# for now exclude split sequences as we likely missed pounds off camera
+m_e2 <- brm(n_pounds ~ Age + item, data = detseq_o[which(detseq_o$split == FALSE),], family = "poisson", iter = 1000, chain = 2, core = 2, backend = "cmdstanr")
+#saveRDS(m_e2, "detailedtools/RDS/m_e2.rds")
 summary(m_e2)
+pp_check(m_e2)
 mcmc_plot(m_e2)
 plot(conditional_effects(m_e2))
 
+# make violin plot
+m_type_pred2 <- m_e2 %>% 
+  epred_draws(newdata = tibble(item = detseq_o$item,
+                               Age = detseq_o$Age))
+
+# age difference in duration to open item
+ggplot(data = m_type_pred2, aes(x = Age, y = .epred)) + geom_violin(aes(color = Age, fill = Age), alpha = 0.4) +
+  stat_summary(detseq_o, inherit.aes = FALSE, mapping=aes(x = Age, y = n_pounds, color = Age), geom = "point", fun = "mean",
+               size = 4) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  guides(color = "none", fill = "none") +
+  labs(x = "Age", y = "Number of pounds required to open item") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
+# item difference in duration to open item
+ggplot(data = m_type_pred2, aes(x = item, y = .epred)) + geom_violin(aes(color = item, fill = item), alpha = 0.4) +
+  stat_summary(detseq_o, inherit.aes = FALSE, mapping=aes(x = item, y = n_pounds, color = item), geom = "point", fun = "mean",
+               size = 4) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  guides(color = "none", fill = "none") +
+  labs(x = "Item type", y = "Number of pounds required to open item") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
 ## Age differences in efficiency (number of mistakes)
-boxplot(detseq_o$n_miss ~ detseq_o$age_of)
-ggplot(detseq_o, aes(x=age_of, y=n_miss)) + 
+boxplot(detseq_o$n_miss ~ detseq_o$Age)
+ggplot(detseq_o, aes(x=Age, y=n_miss)) + 
   geom_violin()
 
-m_e3 <- brm(n_miss ~ age_of, data = detseq_o)
+descdist(detseq_o$n_miss)
+testdist3.1 <- fitdist(detseq_o$n_miss, "pois")
+plot(testdist3.1)
+
+
+m_e3 <- brm(n_miss ~ Age, data = detseq_o, family = "poisson", iter = 2000, chain = 2, core = 2, backend = "cmdstanr", control = list(adapt_delta = 0.99))
+#saveRDS(m_e3, "detailedtools/RDS/m_e3.rds")
 summary(m_e3)
+pp_check(m_e3)
 plot(conditional_effects(m_e3))
 
+# make violin plot
+m_type_pred3 <- m_e3 %>% 
+  epred_draws(newdata = tibble(Age = detseq_o$Age))
+
+# age difference in duration to open item
+ggplot(data = m_type_pred3, aes(x = Age, y = .epred)) + geom_boxplot(aes(color = Age, fill = Age), alpha = 0.4) +
+  stat_summary(detseq_o, inherit.aes = FALSE, mapping=aes(x = Age, y = n_miss, color = Age), geom = "point", fun = "mean",
+               size = 4) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  guides(color = "none", fill = "none") +
+  labs(x = "Age", y = "Average number of mistakes per tool use sequence") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
+# individual variation in how many mistakes are made
+knownids <- data.frame(ID = c("TER", "SPT", "BAL", "TOM", "PEA", "SMG", "ZIM", "MIC", "LAR"))
+for (i in 1:nrow(knownids)) {
+  knownids$nrow[i] <- nrow(detseq_o[which(detseq_o$subjectID == knownids$ID[i]),])
+}
+
+detseq_o2 <- left_join(detseq_o[which(detseq_o$subjectID %in% knownids$ID),], knownids, by = c("subjectID" = "ID"))
+
+ggplot(detseq_o2, aes(x=subjectID, y=n_miss, color = Age, fill = Age)) + 
+  geom_violin(alpha = 0.4) + geom_text(aes(y = 7.5, x = subjectID, label = nrow)) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  labs(x = "Age", y = "Average number of mistakes per tool use sequence") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
 ## Age differences in efficiency (number of repositions)
-boxplot(detseq_o$n_reposit ~ detseq_o$age_of)
-ggplot(detseq_o, aes(x=age_of, y=n_reposit)) + 
+boxplot(detseq_o$n_reposit ~ detseq_o$Age)
+ggplot(detseq_o, aes(x=Age, y=n_reposit)) + 
   geom_violin()
 
 
-m_e4 <- brm(n_reposit ~ age_of, data = detseq_o)
+m_e4 <- brm(n_reposit ~ Age, data = detseq_o, family = "poisson", iter = 2000, chain = 2, core = 2, backend = "cmdstanr")
+#saveRDS(m_e4, "detailedtools/RDS/m_e4.rds")
 summary(m_e4)
 mcmc_plot(m_e4)
-plot(conditional_effects(m_e4))
+pp_check(m_e4)
+plot(conditional_effects(m_e4b))
+
+# make violin plot
+m_type_pred4 <- m_e4 %>% 
+  epred_draws(newdata = tibble(Age = detseq_o$Age))
+
+# age difference in duration to open item
+ggplot(data = m_type_pred4, aes(x = Age, y = .epred)) + geom_violin(aes(color = Age, fill = Age), alpha = 0.4) +
+  stat_summary(detseq_o, inherit.aes = FALSE, mapping=aes(x = Age, y = n_reposit, color = Age), geom = "point", fun = "mean",
+               size = 4) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  guides(color = "none", fill = "none") +
+  labs(x = "Age", y = "Average number of repositions per sequence") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
+# individual variation
+
+ggplot(detseq_o2, aes(x=subjectID, y=n_reposit, color = Age, fill = Age)) + 
+  geom_violin(alpha = 0.4) + geom_text(aes(y = 9.5, x = subjectID, label = nrow)) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  labs(x = "Age", y = "Average number of repositions per tool use sequence") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
 
 ## still account for individual ID (when we can)
+
+
+# some exploration just messing around
+
+library(reshape2)
+melt_detseq <- melt(detseq_o2, measure.vars = c("n_pounds", "n_miss", "n_reposit"))
+
+ggplot(melt_detseq) + geom_violin(aes(y = value, x = variable, color = variable, fill = variable), alpha = 0.4) +
+  stat_summary(melt_detseq, inherit.aes = FALSE, mapping=aes(x = variable, y = value, color = variable), geom = "point", fun = "mean",
+               size = 4) +
+  facet_wrap(~factor(subjectID, levels = c("BAL", "PEA", "TER",  "ZIM", "LAR", "MIC", "SPT", "SMG", "TOM"))) +
+  scale_fill_viridis_d(option = "plasma", end = 0.8) +
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  labs(x = "Age", y = "Average number of repositions per sequence") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
+
+# relationship between nr of pounds and how long the sequence lasts
+ggplot(detseq_o[detseq_o$split == FALSE,], aes(y = seqduration, x = n_pounds, color = Age, shape = Age)) + geom_point(size = 3) + geom_smooth() + 
+  scale_color_viridis_d(option = "plasma", end = 0.8) +
+  labs(y = "Seconds needed to open item", x = "Number of pounds needed to open item") +
+  theme_bw() + theme(axis.text = element_text(size = 12),
+                     axis.title = element_text(size = 14)) 
